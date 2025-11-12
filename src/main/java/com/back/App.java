bbbfe9dab1a15a15c78a93e45f4d19b980654904
jhs512 <jangka512@gmail.com>
2025-11-12 12:30:11 +0900

    WiseSayingControllerTest t4, g

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -1,17 +1,48 @@
 package com.back;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
+    private final Scanner scanner = AppContext.scanner;
+
     public void run() {
         System.out.println("== 명언 앱 ==");
 
-        System.out.print("명령) ");
+        int lastId = 0;
+        List<WiseSaying> wiseSayings = new ArrayList<>();
 
-        System.out.print("명언 : ");
-        System.out.print("작가 : ");
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine().trim();
 
-        System.out.println("1번 명언이 등록되었습니다.");
-        System.out.println("2번 명언이 등록되었습니다.");
+            switch (cmd) {
+                case "종료" -> {
+                    System.out.println("프로그램이 종료되었습니다.");
+                    return;
+                }
+                case "등록" -> {
+                    System.out.print("명언 : ");
+                    String content = scanner.nextLine().trim();
+                    System.out.print("작가 : ");
+                    String author = scanner.nextLine().trim();
 
-        System.out.println("프로그램이 종료되었습니다.");
+                    int id = ++lastId;
+
+                    wiseSayings.add(new WiseSaying(id, content, author));
+
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                }
+                case "목록" -> {
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    for (WiseSaying wiseSaying : wiseSayings.reversed()) {
+                        System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent());
+                    }
+                }
+            }
+        }
     }
 }
--- src/main/java/com/back/AppContext.java
+++ src/main/java/com/back/AppContext.java
@@ -3,7 +3,9 @@ package com.back;
 import java.util.Scanner;
 
 public class AppContext {
-    public static void renew(Scanner scanner) {
+    public static Scanner scanner;
 
+    public static void renew(Scanner scanner) {
+        AppContext.scanner = scanner;
     }
 }
