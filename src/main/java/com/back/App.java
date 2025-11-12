d9a7656ebca0d5165152bfd4b685900689473950
jhs512 <jangka512@gmail.com>
2025-11-12 12:16:04 +0900

    WiseSayingControllerTest t1, g

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -6,6 +6,9 @@ public class App {
 
         System.out.print("명령) ");
 
+        System.out.print("명언 : ");
+        System.out.print("작가 : ");
+
         System.out.println("프로그램이 종료되었습니다.");
     }
 }
