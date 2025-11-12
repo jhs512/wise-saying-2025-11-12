8c58b226bc02733de1440123b4b9533fc3a41b22
jhs512 <jangka512@gmail.com>
2025-11-12 12:16:31 +0900

    WiseSayingControllerTest t2, r

--- src/test/java/com/back/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/back/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -20,4 +20,17 @@ public class WiseSayingControllerTest {
                 .contains("명언 : ")
                 .contains("작가 : ");
     }
+
+    @Test
+    @DisplayName("등록시 생성된 명언번호 노출")
+    void t2() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(rs)
+                .contains("1번 명언이 등록되었습니다.");
+    }
 }
